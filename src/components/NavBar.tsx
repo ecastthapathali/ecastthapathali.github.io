b9e1b4c3ac7cc439f7ed13c68f2a0815686eb90f
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-06-28 22:33:33 +0545

    design change and mobile responsive added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,24 +9,24 @@ const NavBar: React.FC = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 z-10 w-full  h-20">
-      <div className="mx-auto px-4 py-3 flex  justify-between items-center">
-        <a href="/" className="flex items-center">
+    <nav className="fixed top-0 left-0 z-10 w-full h-20 bg-[#00000099] backdrop-blur">
+      <div className="mx-auto py-2 flex justify-between items-center">
+        <a href="/" className="mx-2 flex items-center">
           <img src={Logo} alt="Logo" className="h-14 w-14 mr-2" />
-          <span className="text-white text-4xl font-semibold">ECAST</span>
+          <span className="text-white text-3xl font-semibold">ECAST</span>
         </a>
-        <div className="hidden sm:flex items-center space-x-4">
+        <div className="hidden sm:flex items-center space-x-4 mx-4">
           <NavLink to="/about">About Us</NavLink>
           <NavLink to="/committee">Committee</NavLink>
           <NavLink to="/projects">Projects</NavLink>
           <NavLink to="/gallery">Gallery</NavLink>
         </div>
         <button
-          className="sm:hidden text-white border border-white rounded-full p-2 hover:bg-white hover:text-black transition-colors"
+          className="sm:hidden px-2 m-2 text-[#fff] hover:text-[#dbb732] transition-color duration-500"
           onClick={handleMenuToggle}
         >
           <svg
-            className="h-5 w-5"
+            className="h-8 w-8"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
@@ -35,26 +35,26 @@ const NavBar: React.FC = () => {
               <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M3 6h14v2H3V6zm0 5h14v2H3v-2zm0 5h14v2H3v-2z"
-              />
-            ) : (
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 6h12v2H4V6zm0 5h12v2H4v-2zm0 5h12v2H4v-2z"
+                d="M5 5h21v2H4V6zm0 5h6v2H4v2zm0 5h21v2H4v-2zm0"
+                />
+                ) : (
+                  <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5 5h15v2H4V6zm0 5h15v2H4v2zm0 5h15v2H4v-2zm0"
               />
             )}
           </svg>
         </button>
         {isMenuOpen && (
-        <div className="sm:hidden absolute top-20 right-0 bg-zinc-500/75 p-4 space-y-2 flex flex-col">
+        <div className="sm:hidden fixed top-20 text-center bg-[#00000099] w-full min-h-screen p-4 flex py-48 justify-evenly flex-col">
           <NavLink to="/about">About Us</NavLink>
           <NavLink to="/committee">Committee</NavLink>
           <NavLink to="/projects">Projects</NavLink>
           <NavLink to="/gallery">Gallery</NavLink>
         </div>
       )}
-      </div>
+    </div>
     </nav>
   );
 };
@@ -66,7 +66,7 @@ interface NavLinkProps {
 
 const NavLink: React.FC<NavLinkProps> = ({ to, children }) => {
   return (
-    <a href={to} className="text-white border border-white rounded-full px-4 py-2 hover:bg-white hover:text-black transition-colors">
+    <a href={to} className="text-[#fff] font-bold my-3 px-3 py-2 hover:text-[#dbb732] hover:underline underline-offset-8 transition duration-500">
       {children}
     </a>
   );
