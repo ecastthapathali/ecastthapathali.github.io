0b34b1c32da24efa67cdbddece9f2c227cd335b1
Abhinav Chalise <chalisezabhinav@gmail.com>
2023-07-01 02:47:10 +0545

    formatted all files with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter basename="/ECAST-Web-Frontend">
-				<NavBar />
-				<Routes>
-					<Route path="/" Component={Home} />
-					<Route path="/about" Component={About} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter basename="/ECAST-Web-Frontend">
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
