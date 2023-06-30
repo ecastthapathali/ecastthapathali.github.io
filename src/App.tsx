dff002063b83e3fdf21600ef56e70c4193f086b6
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-06-30 20:43:07 +0545

    basename added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import About from "./components/About";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
+			<BrowserRouter basename="/ECAST-Web-Frontend">
 				<NavBar />
 				<Routes>
 					<Route path="/" Component={Home} />
