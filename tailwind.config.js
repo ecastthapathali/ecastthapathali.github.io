2f89fa5f7971d260c0946e99efd1fe30cd38957c
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-06-30 19:12:24 +0545

    custom style extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,37 @@ export default {
   theme: {
     extend: {},
   },
+  theme: {
+    extend: {
+      brightness: {
+        15: '.15',
+      },
+      fontWeight: {
+        thin: '100',
+      },
+      letterSpacing:
+      {
+        wideext:'2rem',
+      },
+      boxShadow: {
+        'all': '0px 0px 15px 2px rgba(1,1,1, 0.3)',
+      },
+      fontSize:
+      {
+        '9xl': ['9rem'],
+      },
+
+      animation:
+      {
+        texColCha: 'texColCha 5s ease-in infinite'
+      },
+      keyframes: {
+        texColCha: {
+          '0%,100%' : { color:'#dbb732' },
+          '40%,95%': { color:"#ffffff" },
+        }
+      }
+    }
+  },
   plugins: [],
 }
