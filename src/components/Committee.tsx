0f6423677cb3e2180cc89031e4b086c416dea028
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-04 18:18:40 +0545

    mobile responsive added

--- src/components/Committee.tsx
+++ src/components/Committee.tsx
@@ -8,7 +8,7 @@ const Committee = () => {
         <section className="bg-gradient-to-t from-theme-lqa flex w-full text-center flex-col p-4">
             <h1 className="text-theme text-5xl my-8">Our Committee</h1>
             {/* Team Member Cards Collections */}
-            <div className="py-8 grid place-items-center gap-y-16 grid-cols-4">
+            <div className="py-8 grid place-items-center gap-y-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
                 {/* replace it with the appropaite object containing team info... */}
                 {TeamMemberList.map((memInfo)=>(<Card imgSrc="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" name="Name Here" pos="-Position-" />))}
             </div>
