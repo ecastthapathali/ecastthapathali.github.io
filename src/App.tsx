c92a5b964fc07beca9507d0f69a259d4afb27e9a
supremkhatri <suprem.khatri708@gmail.com>
2024-06-01 14:09:44 +0545

    default page correction

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Committee from "./components/Committee";
 
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 // import IntakeForm from "./components/IntakeForm";
 import whatisEcast from "./components/aboutComponents/WhatisECAST";
 import Workshop from "./components/aboutComponents/Workshop";
@@ -13,26 +13,24 @@ import Research from "./components/aboutComponents/Research";
 import OurPastEvents from "./components/ourEvents/OurPastEvents";
 import UpcomingEvents from "./components/ourEvents/UpcomingEvents";
 import WholeEvents from "./components/ourEvents/WholeEvents";
-import Gallery from "./components/Gallery"; 
+import Gallery from "./components/Gallery";
 import FormSub from "./components/FormSub";
 import Projects from "./components/Projects";
 
-
 function App() {
-
   const images = [
-    '/images/1.jpg',
-    '/images/2.jpg',
-    '/images/3.jpg',
-    '/images/4.jpg',
-    '/images/5.jpg',
-    '/images/6.jpg',
-    '/images/7.jpg',
-    '/images/8.jpg',
-    '/images/9.jpg',
-    '/images/10.jpg',
-    '/images/11.jpg',
-    '/images/12.jpg'
+    "/images/1.jpg",
+    "/images/2.jpg",
+    "/images/3.jpg",
+    "/images/4.jpg",
+    "/images/5.jpg",
+    "/images/6.jpg",
+    "/images/7.jpg",
+    "/images/8.jpg",
+    "/images/9.jpg",
+    "/images/10.jpg",
+    "/images/11.jpg",
+    "/images/12.jpg",
   ];
 
   return (
@@ -40,6 +38,7 @@ function App() {
       <BrowserRouter basename="/">
         <NavBar />
         <Routes>
+          <Route path="/" Component={Home} />
           <Route path="/home" Component={Home} />
           <Route path="/about" Component={About} />
           <Route path="/committee" Component={Committee} />
@@ -54,7 +53,6 @@ function App() {
           <Route path="/ourevents" Component={WholeEvents} />
           <Route path="/FormSub" Component={FormSub} />
           <Route path="/Projects" Component={Projects} />
-
         </Routes>
         <Footer />
       </BrowserRouter>
